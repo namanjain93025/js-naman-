4eac2e6ce680b199378aae38dd72e2e027bb5985
namanjain93025 <namanjain93025@gmail.com>
2025-12-18 04:47:53 +0000

    container config done

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as modern js
+
+//alert(3+3);//cant be used nodejs env , but can be used in browser
+console.log(3+3);
+//datatypes in js
+//number => 2 power 53
+//bigint => larger than 2 power 53
+//string  =>"namam"
+//boolean =>true/fasle
+//null =>standalone value
+//undefined =>value not assigned
+//symbol => unique
+//object => key value pair
+console.log(typeof null)//object
+console.log(typeofundefined)//undefined
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,9 @@
+const CustomerId =1223;
+let customerName ="Naman";
+var customerAge =29;
+//customer id is a constant and cant be changed and var is not recommended
+//to be used in modern javas,becoz of its function scope and hoisting isues ;
+customerAdd ="Delhi";
+console.log(CustomerId);
+console.table({customerName,customerAge,customerAdd});
+
